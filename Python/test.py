83223120762ad91324c14ad941b0d8f69c003e72
SO4REN <ale.catinello@gmail.com>
2023-03-05 20:05:53 +0100

    Convert RF and SVM to GPU

--- Python/test.py
+++ Python/test.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
-from sklearn.model_selection import train_test_split, cross_val_score
+
+
+import cudf as cu
+from cuml.svm import SVC as cuSVC, LinearSVC as cuLinearSVC
+from cuml.model_selection import train_test_split as cuTrainTestSplit
+from cuml import RandomForestClassifier as cuRandomForestClassifier
+
 
 #! ------ CALCULATE METRICS -------
-
 def getConfusionMatrix(labels: np.ndarray, predictions: np.ndarray):
     confusion = dict()
     confusion["TP"] = np.sum(labels & predictions)
@@ -24,7 +28,15 @@ def getMetrics(confusion):
     metrics["recall"] = confusion["TP"] / (confusion["TP"] + confusion["FN"])
     metrics["f1"] = 2 * (metrics["precision"] * metrics["recall"]) / (metrics["precision"] + metrics["recall"])
     return metrics
+
+def printMetrics(confusion):
+    print("Accuracy: \t", confusion["accuracy"])
+    print("Precision: \t", confusion["precision"])
+    print("Recall: \t", confusion["recall"])
+    print("F1: \t\t", confusion["f1"])
 #! ---------------------------------
+
+#! ----------- CPU ---------------------
 
 covid = pd.read_parquet('covidClean.parquet')
 
@@ -42,6 +54,27 @@ trainingX = trainingSet.drop(columns = ["AT_RISK"])
 testY = np.array(testSet["AT_RISK"])
 testX = testSet.drop(columns = ["AT_RISK"])
 
+
+#! ------------- GPU ------------------
+
+cuCovid = cu.read_parquet('covidClean.parquet')
+
+toRemove = ["PATIENT_ID", "USMER", "SYMPTOMS_DATE",
+        "MEDICAL_UNIT", "ADMISSION_DATE", "PATIENT_TYPE",
+        "DEATH_DATE", "ORIGIN_COUNTRY"]
+cuCovid = cuCovid.drop(columns = toRemove)
+cuCovid = cuCovid.drop(columns= ["DIED", "INTUBED", "ICU"])
+cuCovid = cuCovid.astype("float32")
+
+labels = cuCovid["AT_RISK"]
+covidX = cuCovid.drop(columns = ["AT_RISK"])
+cuTrainingX, cuTestX, cuTrainingY, cuTestY = cuTrainTestSplit(covidX, labels, test_size=0.25)
+
+cuTestY = cuTestY.to_pandas().to_numpy().astype("bool")
+
+#! -----------------------------------
+
+
 if 0:
 #! ------------- CART ---------------- 
 
@@ -50,22 +83,19 @@ if 0:
     
 #! -----------------------------------
 
-
 if 0:
-#! ------------- RF ------------------
+#! ------------- RF GPU -----------------
+    rf = cuRandomForestClassifier(n_estimators=1000)
 
-    rf = RandomForestClassifier(n_estimators=1000, n_jobs=-1)
+    rf.fit(cuTrainingX, cuTrainingY)
+    predictions = rf.predict(cuTestX)
 
-    rf.fit(trainingX, trainingY)
-    predictions = rf.predict(testX)
+    cuTestY = cuTestY.to_pandas().to_numpy().astype("bool")
+    predictions = predictions.to_pandas().to_numpy().astype("bool")
 
-    metric = getMetrics(getConfusionMatrix(testY, predictions))
-    print("\nRF metrics:")
-    print("Accuracy: \t", metric["accuracy"])
-    print("Precision: \t", metric["precision"])
-    print("Recall: \t", metric["recall"])
-    print("F1: \t\t", metric["f1"])
-
+    metrics = getMetrics(getConfusionMatrix(cuTestY, predictions))
+    print("\nRF metrics (GPU):")
+    printMetrics(metrics)
 #! -----------------------------------
 
 
@@ -77,13 +107,10 @@ if 0:
     nb.fit(trainingX, trainingY)
     predictions = nb.predict(testX)
     
-    metric = getMetrics(getConfusionMatrix(testY, predictions))
+    metrics = getMetrics(getConfusionMatrix(testY, predictions))
 
     print("\nNB Gaussian metrics:")
-    print("Accuracy: \t", metric["accuracy"])
-    print("Precision: \t", metric["precision"])
-    print("Recall: \t", metric["recall"])
-    print("F1: \t\t", metric["f1"])
+    printMetrics(metrics)
 
 #! -----------------------------------
 
@@ -95,13 +122,10 @@ if 0:
     nb.fit(trainingX, trainingY)
     predictions = nb.predict(testX)
 
-    metric = getMetrics(getConfusionMatrix(testY, predictions))
+    metrics = getMetrics(getConfusionMatrix(testY, predictions))
 
     print("\nNB Multinomial metrics:")
-    print("Accuracy: \t", metric["accuracy"])
-    print("Precision: \t", metric["precision"])
-    print("Recall: \t", metric["recall"])
-    print("F1: \t\t", metric["f1"])
+    printMetrics(metrics)
 
 #! -----------------------------------
 
@@ -114,3 +138,39 @@ if 0:
 
 #! -----------------------------------
 
+
+if 0:
+#! ------------- LINEAR SVM ------------------
+
+    svm = cuLinearSVC(max_iter=100000)
+
+    svm.fit(cuTrainingX, cuTrainingY)
+    predictions = svm.predict(cuTestX)
+    
+    predictions = predictions.to_pandas().to_numpy().astype("bool")
+
+    metrics = getMetrics(getConfusionMatrix(cuTestY, predictions))
+
+    print("\nLinear SVM metrics (GPU):")
+    printMetrics(metrics)
+
+#! -----------------------------------
+
+if 1:
+#! ------------- SVM ------------------
+
+    results = dict()
+    kernels = ["poly", "rbf", "sigmoid"]
+
+    for kernel in kernels:
+        svm = cuSVC(kernel=kernel, max_iter=10)
+        svm.fit(cuTrainingX, cuTrainingY)
+        predictions = svm.predict(cuTestX)
+        
+        predictions = predictions.to_pandas().to_numpy().astype("bool")
+
+        metrics = getMetrics(getConfusionMatrix(cuTestY, predictions))
+        print("\n SVM con kernel '", kernel, "' metrics: ")
+        printMetrics(metrics)
+
+#! -----------------------------------
